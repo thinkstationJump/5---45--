1cc3644c25d299a3c4230c0670bac12334218af7
onej <xiaomage@imac.local>
2015-11-05 15:33:25 +0800

    5期-百思不得姐02-自定义UITabBarController

--- 5期-百思不得姐/AppDelegate.m
+++ 5期-百思不得姐/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "XMGTabBarController.h"
 
 @interface AppDelegate ()
 
@@ -16,76 +17,17 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    /** 文字属性 **/
-    // 普通状态下的文字属性
-    NSMutableDictionary *normalAttrs = [NSMutableDictionary dictionary];
-    normalAttrs[NSFontAttributeName] = [UIFont systemFontOfSize:14];
-    normalAttrs[NSForegroundColorAttributeName] = [UIColor grayColor];
-    // 选中状态下的文字属性
-    NSMutableDictionary *selectedAttrs = [NSMutableDictionary dictionary];
-    selectedAttrs[NSForegroundColorAttributeName] = [UIColor darkGrayColor];
-    
     // 创建窗口
     self.window = [[UIWindow alloc] init];
     self.window.frame = [UIScreen mainScreen].bounds;
     
     // 设置根控制器
-    UITabBarController *tabBarVc = [[UITabBarController alloc] init];
-    
-    UITableViewController *vc0 = [[UITableViewController alloc] init];
-    vc0.view.backgroundColor = [UIColor redColor];
-    vc0.tabBarItem.title = @"精华";
-    [vc0.tabBarItem setTitleTextAttributes:normalAttrs forState:UIControlStateNormal];
-    [vc0.tabBarItem setTitleTextAttributes:selectedAttrs forState:UIControlStateSelected];
-    vc0.tabBarItem.image = [UIImage imageNamed:@"tabBar_essence_icon"];
-    vc0.tabBarItem.selectedImage = [UIImage imageNamed:@"tabBar_essence_click_icon"];
-    [tabBarVc addChildViewController:vc0];
-    
-    UIViewController *vc1 = [[UIViewController alloc] init];
-    vc1.view.backgroundColor = [UIColor blueColor];
-    vc1.tabBarItem.title = @"新帖";
-    [vc1.tabBarItem setTitleTextAttributes:normalAttrs forState:UIControlStateNormal];
-    [vc1.tabBarItem setTitleTextAttributes:selectedAttrs forState:UIControlStateSelected];
-    vc1.tabBarItem.image = [UIImage imageNamed:@"tabBar_new_icon"];
-    vc1.tabBarItem.selectedImage = [UIImage imageNamed:@"tabBar_new_click_icon"];
-    [tabBarVc addChildViewController:vc1];
-    
-    UITableViewController *vc2 = [[UITableViewController alloc] init];
-    vc2.view.backgroundColor = [UIColor greenColor];
-    vc2.tabBarItem.title = @"关注";
-    [vc2.tabBarItem setTitleTextAttributes:normalAttrs forState:UIControlStateNormal];
-    [vc2.tabBarItem setTitleTextAttributes:selectedAttrs forState:UIControlStateSelected];
-    vc2.tabBarItem.image = [UIImage imageNamed:@"tabBar_friendTrends_icon"];
-    vc2.tabBarItem.selectedImage = [UIImage imageNamed:@"tabBar_friendTrends_click_icon"];
-    [tabBarVc addChildViewController:vc2];
-    
-    UIViewController *vc3 = [[UIViewController alloc] init];
-    vc3.view.backgroundColor = [UIColor grayColor];
-    vc3.tabBarItem.title = @"我";
-    [vc3.tabBarItem setTitleTextAttributes:normalAttrs forState:UIControlStateNormal];
-    [vc3.tabBarItem setTitleTextAttributes:selectedAttrs forState:UIControlStateSelected];
-    vc3.tabBarItem.image = [UIImage imageNamed:@"tabBar_me_icon"];
-    vc3.tabBarItem.selectedImage = [UIImage imageNamed:@"tabBar_me_click_icon"];
-    [tabBarVc addChildViewController:vc3];
-    
-    self.window.rootViewController = tabBarVc;
+    self.window.rootViewController = [[XMGTabBarController alloc] init];
     
     // 显示窗口
     [self.window makeKeyAndVisible];
     return YES;
 }
-
-//    UIButton *btn = [UIButton buttonWithType:UIButtonTypeSystem];
-//    btn.frame = CGRectMake(100, 100, 100, 100);
-//    btn.backgroundColor = [UIColor whiteColor];
-//    [btn setImage:[[UIImage imageNamed:@"tabBar_essence_icon"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal] forState:UIControlStateNormal];
-//    [vc0.view addSubview:btn];
-
-//    // 加载图片
-//    UIImage *tempImage = [UIImage imageNamed:@"tabBar_essence_click_icon"];
-//    // 产生一张不会进行自动渲染的图片
-//    UIImage *selectedImage = [tempImage imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
-//    vc0.tabBarItem.selectedImage = selectedImage;
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
