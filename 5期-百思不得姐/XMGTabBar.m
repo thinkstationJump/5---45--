d0ec0e5281635de6453b0d695c40698d8886cdb3
onej <xiaomage@imac.local>
2015-11-06 10:43:02 +0800

    5期-百思不得姐05-自定义UITabbar

---
+++ 5期-百思不得姐/XMGTabBar.m
@@ -0,0 +1,80 @@
+//
+//  XMGTabBar.m
+//  5期-百思不得姐
+//
+//  Created by xiaomage on 15/11/6.
+//  Copyright © 2015年 xiaomage. All rights reserved.
+//
+
+#import "XMGTabBar.h"
+
+@interface XMGTabBar()
+/** 中间的发布按钮 */
+@property (nonatomic, weak) UIButton *publishButton;
+@end
+
+@implementation XMGTabBar
+
+#pragma mark - 懒加载
+/** 发布按钮 */
+- (UIButton *)publishButton
+{
+    if (!_publishButton) {
+        UIButton *publishButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        publishButton.backgroundColor = XMGRandomColor;
+        [publishButton setImage:[UIImage imageNamed:@"tabBar_publish_icon"] forState:UIControlStateNormal];
+        [publishButton setImage:[UIImage imageNamed:@"tabBar_publish_click_icon"] forState:UIControlStateHighlighted];
+        [publishButton addTarget:self action:@selector(publishClick) forControlEvents:UIControlEventTouchUpInside];
+        [self addSubview:publishButton];
+        _publishButton = publishButton;
+    }
+    return _publishButton;
+}
+
+#pragma mark - 初始化
+/**
+ *  布局子控件
+ */
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    
+    // NSClassFromString(@"UITabBarButton") == [UITabBarButton class]
+    // NSClassFromString(@"UIButton") == [UIButton class]
+    
+    /**** 设置所有UITabBarButton的frame ****/
+    // 按钮的尺寸
+    CGFloat buttonW = self.frame.size.width / 5;
+    CGFloat buttonH = self.frame.size.height;
+    CGFloat buttonY = 0;
+    // 按钮索引
+    int buttonIndex = 0;
+    
+    for (UIView *subview in self.subviews) {
+        // 过滤掉非UITabBarButton
+//        if (![@"UITabBarButton" isEqualToString:NSStringFromClass(subview.class)]) continue;
+        if (subview.class != NSClassFromString(@"UITabBarButton")) continue;
+        
+        // 设置frame
+        CGFloat buttonX = buttonIndex * buttonW;
+        if (buttonIndex >= 2) { // 右边的2个UITabBarButton
+            buttonX += buttonW;
+        }
+        subview.frame = CGRectMake(buttonX, buttonY, buttonW, buttonH);
+        
+        // 增加索引
+        buttonIndex++;
+    }
+    
+    /**** 设置中间的发布按钮的frame ****/
+    self.publishButton.frame = CGRectMake(0, 0, buttonW, buttonH);
+    self.publishButton.center = CGPointMake(self.frame.size.width * 0.5, self.frame.size.height * 0.5);
+}
+
+#pragma mark - 监听
+- (void)publishClick
+{
+    XMGLogFunc
+}
+
+@end
